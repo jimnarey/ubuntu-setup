2e88afff9f2926a594218f42519cd04ebd64940d
jimnarey <jimnarey@me.com>
2019-05-02 00:00:21 +0100

    Play with sudo options in test.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
+_DEBIAN_FRONTEND=$DEBIAN_FRONTEND
+export DEBIAN_FRONTEND=noninteractive
+
+
 mkdir $HOME/.ubstemp
 cd $HOME/.ubstemp
+
+#Check -y flag for upgrade and dist-upgrade
 
 #Main package install
 apt-get update
 apt-get upgrade -y
 apt-get distupgrade -y
 apt-get install -y build-essential git gparted python python3 python-pip python3-pip zsh openssh-server \
-        wget curl mount-img nano vim terminator fonts-powerline
+        wget curl mount-img nano vim terminator fonts-powerline exfat-fuse exfat-utils tmux
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
 #Zsh Setup
-sudo -H $SUDO_USER cp $HOME/.bashrc $HOME/.zshrc
-sudo -H $SUDO_USER chsh -s $(which zsh)
+sudo -u $SUDO_USER -H $SUDO_USER cp $HOME/.bashrc $HOME/.zshrc
+# Check about generating zsh default config
+sudo -u $SUDO_USER chsh -s $(which zsh)
 chsh -s $(which zsh)
-sudo -H $SUDO_USER sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
+sudo -u $SUDO_USER sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# Remove Ruby from prompt
 
 #Ruby Install
 sudo -H $SUDO_USER wget -q https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer -O- | bash
@@ -53,7 +60,7 @@ cp *.list /etc/apt/sources.list.d/
 curl https://apt.galliumos.org/galliumos.gpg | apt-key add
 apt update
 apt dist-upgrade -y
-sed -i 's/900/1/' /etc/apt/preferences.d/galliumos.pref
+sed -i 's/900/1/' /etc/apt/preferences.d/galliumos.pref # Test this
 apt dist-upgrade -y
 apt install -y linux-image-galliumos-braswell galliumos-braswell
 apt purge linux*generic
@@ -61,6 +68,11 @@ update-grub
 
 #Crouton chroot only
 # Map shared drives
+# What else?
+
+#Setup Terminator
+# Solarised, font size
+
 
 #Cleanup
 
@@ -69,3 +81,4 @@ apt-get clean
 apt-get autoclean
 cd ../
 rm -rf ./.ubstemp
+export DEBIAN_FRONTEND=$_DEBIAN_FRONTEND
--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-echo Moo
+echo $USER
 echo $SUDO_USER
 echo $HOME
+sudo -u $SUDO_USER echo $HOME
 sudo -H $SUDO_USER echo $HOME
+sudo -u $SUDO_USER -H $SUDO_USER echo $HOME
